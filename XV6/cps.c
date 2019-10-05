2afad37707628dbed2f644a8fb67129f9cd2a441
Hrishikesh V <38471846+HrishikeshVish@users.noreply.github.com>
2019-10-05 12:24:14 +0530

    Add files via upload
    XV6 with the implementation of ps system call

---
+++ XV6/cps.c
@@ -0,0 +1,17 @@
+#include "types.h"
+#include "defs.h"
+#include "param.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "x86.h"
+#include "proc.h"
+#include "spinlock.h"
+
+int main()
+{
+
+
+cps();
+
+return 0;
+} 
